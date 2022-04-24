fdf189cc5571d7e451facbbcc83804c362178f8f
IM94 <hereimenkov@gmail.com>
2022-04-24 23:23:16 +0300

    Created a pages folder, base_page.py, main_page.py with first methods.

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,11 @@
 import pytest
+from pages import MainPage
 
 LINK = 'http://selenium1py.pythonanywhere.com/'
 LOGIN_LINK_ID = '#login_link'
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(LINK)
-    login_link = browser.find_element_by_css_selector(LOGIN_LINK_ID)
-    login_link.click()
+    page = MainPage(browser, LINK)
+    page.open()
+    page.go_to_login_page()
